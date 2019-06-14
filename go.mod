415c8487c767cb4ff4d294f54f5b43e6940b4050
Piszmog <piszmogcloud@gmail.com>
2019-06-14 13:38:08 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module numberToWord
+
+go 1.12
+
+require github.com/pkg/errors v0.8.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(number(1000))
+	fmt.Println(number(1012))
+	fmt.Println(number(1055))
+	fmt.Println(number(9099))
+	fmt.Println(number(99999))
+}
